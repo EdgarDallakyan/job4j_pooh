88fa55b3d20ca6c03d543f4ad7e55bbc3088b0e7
edgar <edgar_dallakyan@mail.ru>
2025-04-02 16:42:11 +0300

    3. Тестовое задание - проект "Pooh JMS" [#268841 #538363]

--- src/test/java/ru/job4j/pooh/TopicSchemaTest.java
+++ src/test/java/ru/job4j/pooh/TopicSchemaTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.pooh;
 
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
@@ -8,7 +7,6 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 
 class TopicSchemaTest {
     @Test
